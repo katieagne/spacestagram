8695557cbd94b4a658425c868535bb8c3d971082
katieagne <katieagne@gmail.com>
2021-09-21 19:58:10 -0700

    Update Readme

--- src/util.js
+++ src/util.js
@@ -1,2 +1,3 @@
-export const API_KEY = process.env.REACT_APP_NASA_API_KEY;
+import NASA_API_KEY from "./apikey";
+export const API_KEY = NASA_API_KEY;
 export const API_URL = "https://api.nasa.gov/planetary/apod";
